07874d8917ceaa455fa15a5efed893355232b9a5
Emma Karlsson <emma@Emmas-MacBook-Pro.local>
2021-05-02 11:41:16 +0200

    all auth is set up and works

--- components/Menu.js
+++ components/Menu.js
@@ -1,25 +1,61 @@
 import React from 'react';
+
 import styled from 'styled-components';
+import { useAuth } from '../contexts/AuthContext';
+import { useRouter } from 'next/router';
+
+import Link from 'next/link';
 
 const StyledSection = styled.section`
     width: 100vw;
     height: 100vh;
+
     position: absolute;
     top: 0;
     left: 0;
+
     background-color: green;
+
     display: none;
 
     &.open {
-        display: block;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
     }
+`;
+
+
+const StyledLink = styled.a`
+    cursor: pointer;
 `;
 
 const Menu = () => {
 
+    const { logout } = useAuth();
+    const router = useRouter();
+
+    const handleLogOut = async () => {
+        try {
+            await logout();
+            console.log('successful logout')
+            router.push('/login');
+        } catch (e) {
+            console.log('Failed to log out', e)
+        }
+    };
+
     return(
         <StyledSection id='menu'>
-            hej
+            <Link href='/home'>
+                <StyledLink>Feed</StyledLink>
+            </Link>
+            <Link href='/profile'>
+                <StyledLink>Profile</StyledLink>
+            </Link>
+            <StyledLink onClick={handleLogOut}>Log Out</StyledLink>
+           
         </StyledSection>
     )
 }
--- pages/login.js
+++ pages/login.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styled from 'styled-components';
 import { useForm } from 'react-hook-form';
+import { useRouter } from 'next/router';
+import { useAuth } from '../contexts/AuthContext';
 
 import InputField from '../components/InputField';
 import { StyledForm } from '../components/StyledForm';
+import { FormBtn } from '../components/Buttons/FormBtn';
 
 const LogIn = () => {
 
-    const { register, handleSubmit, formState: { errors } } = useForm({
+    const { login } = useAuth();
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
-    })
+    const router = useRouter();
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
 
     const onSubmit = async (data) => {
-        console.log(data);
+        try {
+            setError('')
+            setLoading(true)
+            const user = await login(data.email, data.password)
+            console.log('SUCCESSful login!!', user.user.email)
+            router.push('/home')
+        } catch (error) {
+            setError('Failed to log in', error)
+            console.log(error)
+        }
+        setLoading(false)
     }
 
     return(
         <main>
-            <StyledForm>
+            <StyledForm onSubmit={handleSubmit(onSubmit)}>
+                {error && <p>{error}</p>}
                 <InputField 
                     inputName='email'
                     inputType='email'
@@ -31,6 +48,7 @@ const LogIn = () => {
                     labelText='Password:'
                     register={register}
                 />
+                <FormBtn typ='submit'>Log in</FormBtn>
             </StyledForm>
         </main>
 
